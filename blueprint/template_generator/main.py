b78b01ef5406ed36a90d0998f1e8d718e2ea5d53
Likianta <likianta@foxmail.com>
2021-03-26 17:53:31 +0800

    Update template_generator

--- blueprint/template_generator/main.py
+++ blueprint/template_generator/main.py
@@ -1,13 +1,4 @@
 """
-class {WidgetName}({Parent}):
-    {props_type_hint}
-    
-    def _init_properties(self):
-        {props_init}
-    
-    {on_props_changed}
-    
-    {on_signals_emit}
 
 """
 
--- blueprint/template_generator/sidework.py
+++ blueprint/template_generator/sidework.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 from lk_logger import lk
 from lk_utils.read_and_write import loads, dumps
 
@@ -12,31 +14,33 @@ def static_qml_basic_types(file_i, file_o):
                     qmltype: {
                         'parent': ...,
                         'props': {prop: type, ...}
-                                        ^--^ 我们统计的是这个.
-                    }, ...
+                    }, ...              ^--^ 我们统计的是这个.
                 }, ...
             }
         file_o: *.txt or empty str. the empty string means 'donot dump to file,
             just print it on the console'.
             structure: [type, ...]. 一个去重后的列表, 按照字母表顺序排列.
+            
+    Outputs:
+        data_w: {type: [(module, qmltype, prop), ...], ...}
     """
     data_r = loads(file_i)
-    data_w = set()
+    data_w = defaultdict(set)  # type: dict[str, set[tuple[str, str, str]]]
     
-    for v1 in data_r.values():
-        for v2 in v1.values():
-            for v3 in v2['props'].values():
-                type_ = v3
-                data_w.add(type_)
+    for k1, v1 in data_r.items():
+        for k2, v2 in v1.items():
+            for k3, v3 in v2['props'].items():
+                # k1: module; k2: qmltype; k3: prop; v3: type
+                data_w[v3].add((k1, k2, k3))
                 
-    data_w = sorted(data_w)
     if file_o == '':
-        lk.logp(data_w)
+        lk.logp(sorted(data_w.keys()))
     else:
+        data_w = {k: sorted(data_w[k]) for k in sorted(data_w.keys())}
         dumps(data_w, file_o)
 
 
 if __name__ == '__main__':
     static_qml_basic_types(
-        '../resources/no5_all_qml_props.json', ''
+        '../resources/no5_all_qml_props.json', '../../tests/test.json'
     )
