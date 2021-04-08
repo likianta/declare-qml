8abefbb34dab39ef7278130109096052e54c504e
Likianta <likianta@foxmail.com>
2021-04-08 19:38:34 +0800

    Avoid importing Application from components module

--- pyml_rc/declare_qt/__init__.py
+++ pyml_rc/declare_qt/__init__.py
@@ -1,1 +1,9 @@
-__version__ = '0.0.1'
+from .components import *
+
+# note that we delete Application from `.qt_widgets`, becase we would like to
+# use `..application.Application` instead.
+del Application
+
+from .application import Application
+
+__version__ = '0.1.0'
--- pyml_rc/declare_qt/components/base_component.py
+++ pyml_rc/declare_qt/components/base_component.py
@@ -3,6 +3,11 @@ from declare_foundation.components import BaseComponent as _Base
 
 class BaseComponent(_Base):
     
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        super(BaseComponent, self).__exit__(exc_type, exc_val, exc_tb)
+        if hasattr(self, 'show'):
+            self.show()
+    
     def _add_self_to_parent(self, child_com, parent_com):
         if parent_com is not None:
             child_com.setParent(parent_com)
