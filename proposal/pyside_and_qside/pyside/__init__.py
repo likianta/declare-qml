26b516753dc229ae443972a002a61b0aa196aaa2
Likianta <likianta@foxmail.com>
2021-10-05 13:22:55 +0800

    Rename 'py_side' to 'pyside'

---
+++ proposal/pyside_and_qside/pyside/__init__.py
@@ -0,0 +1,10 @@
+"""
+Python Side Handlers.
+See also Qt Modeling Side Handlers at `../qmside`.
+
+Note this package is mostly copied from my another project `lk-qtquick-scaffold`.
+"""
+from .application import Application
+from .application import app
+from .pyside import pyside
+from .pyside import reg
